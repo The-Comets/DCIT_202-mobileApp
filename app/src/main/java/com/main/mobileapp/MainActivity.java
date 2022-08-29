63ddc67f9ef169fa4e998f3433757313c660bb3d
David-Main <work.davidmainoo@gmail.com>
2022-08-29 00:06:05 +0000

    copied kofi's project into a new project and is now working on mine

---
+++ app/src/main/java/com/main/mobileapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.main.mobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        ImageView cartBtn = (ImageView) findViewById(R.id.cartButton);
+//        cartBtn.setOnClickListener(new View.OnClickListener() {
+//
+//        });
+
+    }
+}
